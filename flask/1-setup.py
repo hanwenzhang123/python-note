834d98685c9889fbfd73a35944e5f3653f30f751
Hanwen Zhang <hanwenzhang888@gmail.com>
2021-04-22 10:47:30 -0400

    Create 1-setup.py

---
+++ flask/1-setup.py
@@ -0,0 +1,19 @@
+View: A view is a function that returns an HTTP response. This response has to be a string but can be any string you want.
+Route: A route is the URL path to a view. They always start with a forward slash / and can end with one if you want.
+
+
+#create an app:
+
+#Import Flask from the flask library
+from flask import Flask #in python we always name class with an upper case
+#Create a new Flask instance as a variable named app. The name you pass to the Flask app should be __name__
+app = Flask(__name__) # #use our current namespace is
+
+#create a view:
+
+#Add a view function named index. Give this view a route of "/". 
+#Make the view return your name. You do not need to use app.run().
+@app.route('/')  #route gives it a view after the '/'. #a function that wrap around another function, let you do things the function
+def index():
+    return 'Hanwen Zhang'
+  
