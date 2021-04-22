cde068996430fb48db5929d35c0bcdc6989a87d3
Hanwen Zhang <hanwenzhang888@gmail.com>
2021-04-21 22:27:55 -0400

    Create 7-lambda.py

---
+++ functional-python/7-lambda.py
@@ -0,0 +1,3 @@
+lambda, like def, is the keyword that marks a new function. Lambda functions don't have to have a name, though.
+
+Lambdas can't contain new lines (outside of containers) or assignments.
