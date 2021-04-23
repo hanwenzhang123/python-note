4f3a5e8e7f0fdd6fb7b3241d47ff7f4ae3a52d64
Hanwen Zhang <hanwenzhang888@gmail.com>
2021-04-22 21:44:06 -0400

    Create 11-flash.py

---
+++ flask/11-flash.py
@@ -0,0 +1,27 @@
+flash(): This function stores a message in the session that will self-destruct after the response is returned.
+
+get_flashed_messages(): This function gets all of the flash messages stored in the session.
+
+app.secret_key: This configuration attribute stores the secret key that all messages are cryptographically signed with.
+
+{% with %}: The Flask template version of Python's with block. Let's you temporarily define a variable. Must be closed with {% endwith %}.
+  
+  
+  
+  #Import flash from Flask.
+#Add a secret_key attribute to the app object.
+#flash() a message in the fishy() view before the return. The message can have any content you want.
+from flask import Flask, redirect, url_for, render_template, flash
+
+app = Flask(__name__)
+app.secret_key = 'dsfjhkshfjkhdfjaskhkaajkf' #better when it is random
+
+
+@app.route('/')
+def index():
+    return render_template("index.html")
+
+@app.route('/fishy')
+def fishy():
+    flash("Hello")
+    return redirect(url_for('index'))
