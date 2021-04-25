d88ec81585592f6d1aef640eb883d4c9d544d6f0
Hanwen Zhang <hanwenzhang888@gmail.com>
2021-04-25 11:36:54 -0400

    Create 1-solid-path.py

---
+++ file-system/1-solid-path.py
@@ -0,0 +1,38 @@
+import pathlib
+
+def get_root():
+  root = pathlib.PurePath(
+    input("What is the full path where you would like the project?")
+  )
+  if not root.is_absolute():
+    return get_root()
+  return root
+
+def main():
+  project_root = get_root()
+  project_name = None
+  while not project_name:
+    project_name = input("what is the full name for the project?").strip()
+    print("creating {} in {}".format(project_name, project_root))
+  
+if __name__ == '__main__':
+  main()
+  
+  
+  #solid path
+  
+ #I'd like you to change how get_root works. 
+#I still want to ask for a path but if the path is relative, change it into an absolute path. 
+#You can assume that the path is relative from the current working directory. 
+#The function should always return an absolute path.
+
+import pathlib
+
+
+def get_root():
+    root = pathlib.PurePath(
+        input("What's the full path where you'd like the project? ")
+    )
+    if not root.is_absolute():
+        return get_root()
+    return root
