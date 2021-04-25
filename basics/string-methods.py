e7daa577702bf9a288bf4d81bdfa2dd37ddb031a
Hanwen Zhang <hanwenzhang888@gmail.com>
2021-04-25 18:16:28 -0400

    Update string-methods.py

--- basics/string-methods.py
+++ basics/string-methods.py
@@ -110,3 +110,12 @@ print(reverse_string("Hello world!")) # should print !dlrow olleH
 print(reverse_string(""))# 
 
 
+
+#Make Spoonerism
+#Spoonerism is made when someone says “Belly Jeans” instead of “Jelly Beans”.
+def make_spoonerism(word1, word2):
+  return word2[0]+word1[1:]+" "+word1[0]+word2[1:]
+
+print(make_spoonerism("Codecademy", "Learn")) # should print Lodecademy Cearn
+print(make_spoonerism("Hello", "world!")) # should print wello Horld!
+print(make_spoonerism("a", "b")) # should print b a
