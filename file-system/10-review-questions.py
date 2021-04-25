07ea357f8280c800814f1998133a571ff735ed85
Hanwen Zhang <hanwenzhang888@gmail.com>
2021-04-25 14:43:04 -0400

    Update 10-review-questions.py

--- file-system/10-review-questions.py
+++ file-system/10-review-questions.py
@@ -22,3 +22,21 @@ What directory does .. represent?
 
 Which of these is likely the root directory on a Windows machine?
 F:\
+  
+  I want to change a file's filename. Which method should I use?
+  os.rename
+  
+  Which method should you use if you need to create a path but one or more directories in it might not already exist?
+  os.makedirs
+  
+  When using os.makedirs, an error is thrown if the desired directory already exists. How do you prevent that?
+  exist_ok=True
+  
+  
+  I'm using a temporary directory inside of a function. At what point would the directory, and any contents it may have, be deleted?
+  When the function is finished
+  
+  
+ When you delete a file with os.remove, that file is not placed in the system trash.
+
+Youu are not guaranteed to have a visible (in the file system) file when using tempfile.TemporaryFile
