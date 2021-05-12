61dc4fa82384140b82bc9a5824a0ce56ad58232d
Hanwen Zhang <hanwenzhang888@gmail.com>
2021-05-12 12:55:57 -0400

    Add files via upload

---
+++ SQLAlchemy/1-local-setup.py
@@ -0,0 +1,23 @@
+ORM - object relational mapper
+create, read, update, delete data instead of writing SQL
+
+Local Setup Steps
+
+Create a folder for your project
+Open your folder in your IDE
+Open the terminal in your IDE
+
+Install virtual environment
+Mac: python3 -m pip install --user virtualenv
+Windows: py -m pip install --user virtualenv
+
+Create a virtual environment
+Mac: python3 -m venv env
+Windows: python -m venv env
+
+Activate your environment
+Mac: source ./env/bin/activate
+Windows: .\env\Scripts\activate
+
+Install SQLAlchemy pip install sqlalchemy
+Create requirements file pip freeze > requirements.txt
