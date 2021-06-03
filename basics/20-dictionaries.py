7d5a25937f1eaae59fcc04b0d53ec8bc13b8a65c
Hanwen Zhang <hanwenzhang888@gmail.com>
2021-06-03 17:53:53 -0400

    Create 20-dictionaries.py

---
+++ basics/20-dictionaries.py
@@ -0,0 +1,85 @@
+#Get A Key
+
+zodiac_elements = {"water": ["Cancer", "Scorpio", "Pisces"], "fire": ["Aries", "Leo", "Sagittarius"], "earth": ["Taurus", "Virgo", "Capricorn"], "air":["Gemini", "Libra", "Aquarius"]}
+
+print(zodiac_elements['earth'])
+print(zodiac_elements['fire'])
+
+['Taurus', 'Virgo', 'Capricorn']
+['Aries', 'Leo', 'Sagittarius']
+
+
+
+#Try/Except to Get a Key
+
+caffeine_level = {"espresso": 64, "chai": 40, "decaf": 0, "drip": 120}
+
+caffeine_level['matcha'] = 30
+
+try:
+  print(caffeine_level['matcha'])
+except KeyError:
+  print("Unknown Caffeine Level")
+ 
+
+
+#Delete a Key
+#.pop() works to delete items from a dictionary, when you know the key value.
+
+available_items = {"health potion": 10, "cake of the cure": 5, "green elixir": 20, "strength sandwich": 25, "stamina grains": 15, "power stew": 30}
+health_points = 20
+
+health_points += available_items.pop("stamina grains", 0)
+health_points += available_items.pop("power stew", 0)
+health_points += available_items.pop("mystic bread", 0)
+
+print(available_items)
+print(health_points)
+
+{'health potion': 10, 'cake of the cure': 5, 'green elixir': 20, 'strength sandwich': 25}
+65
+
+
+
+#Get All Keys
+user_ids = {"teraCoder": 100019, "pythonGuy": 182921, "samTheJavaMaam": 123112, "lyleLoop": 102931, "keysmithKeith": 129384}
+num_exercises = {"functions": 10, "syntax": 13, "control flow": 15, "loops": 22, "lists": 19, "classes": 18, "dictionaries": 18}
+
+users = user_ids.keys()
+lessons = num_exercises.keys()
+
+print(users)
+print(lessons)
+
+dict_keys(['teraCoder', 'pythonGuy', 'samTheJavaMaam', 'lyleLoop', 'keysmithKeith'])
+dict_keys(['functions', 'syntax', 'control flow', 'loops', 'lists', 'classes', 'dictionaries'])
+
+
+#Get All Values
+
+num_exercises = {"functions": 10, "syntax": 13, "control flow": 15, "loops": 22, "lists": 19, "classes": 18, "dictionaries": 18}
+
+total_exercises = 0
+
+for exercises in num_exercises.values():
+  total_exercises += exercises
+  
+print(total_exercises)  #115
+
+
+
+#Get All Items
+#You can get both the keys and the values with the .items() method.
+
+biggest_brands = {"Apple": 184, "Google": 141.7, "Microsoft": 80, "Coca-Cola": 69.7, "Amazon": 64.8}
+ 
+for company, value in biggest_brands.items():
+  print(company + " has a value of " + str(value) + " billion dollars. ")
+
+Apple has a value of 184 billion dollars.
+Google has a value of 141.7 billion dollars.
+Microsoft has a value of 80 billion dollars.
+Coca-Cola has a value of 69.7 billion dollars.
+Amazon has a value of 64.8 billion dollars.
+
+ 
