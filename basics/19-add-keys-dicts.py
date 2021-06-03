5389a8c72bd8cb2cb4bfc8d2f6e54f765b5063ad
Hanwen Zhang <hanwenzhang888@gmail.com>
2021-06-03 17:36:53 -0400

    Create 19-add-keys-dicts.py

---
+++ basics/19-add-keys-dicts.py
@@ -0,0 +1,19 @@
+To add a single key: value pair to a dictionary, we can use the syntax:
+
+dictionary[key] = value
+
+menu = {"oatmeal": 3, "avocado toast": 6, "carrot juice": 5, "blueberry muffin": 2}
+
+menu["cheesecake"] = 8
+
+{"oatmeal": 3, "avocado toast": 6, "carrot juice": 5, "blueberry muffin": 2, "cheesecake": 8}
+
+
+animals_in_zoo = {}
+animals_in_zoo['zebras'] = 8
+animals_in_zoo['monkeys'] = 12
+animals_in_zoo['dinosaurs'] = 0
+print(animals_in_zoo)
+
+
+{'zebras': 8, 'monkeys': 12, 'dinosaurs': 0}
