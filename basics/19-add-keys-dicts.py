54acbf43b8c432fca3a6fc5971401d43182246a7
Hanwen Zhang <hanwenzhang888@gmail.com>
2021-06-03 17:38:45 -0400

    Update 19-add-keys-dicts.py

--- basics/19-add-keys-dicts.py
+++ basics/19-add-keys-dicts.py
@@ -17,3 +17,19 @@ print(animals_in_zoo)
 
 
 {'zebras': 8, 'monkeys': 12, 'dinosaurs': 0}
+
+
+#Add Multiple Keys
+#we can use the .update() method.
+
+
+sensors = {"living room": 21, "kitchen": 23, "bedroom": 20}
+sensors.update({"pantry": 22, "guest room": 25, "patio": 34})
+{"living room": 21, "kitchen": 23, "bedroom": 20, "pantry": 22, "guest room": 25, "patio": 34}
+
+
+user_ids = {"teraCoder": 9018293, "proProgrammer": 119238}
+user_ids.update({"theLooper": 138475, "stringQueen": 85739})
+print(user_ids)
+
+{'teraCoder': 9018293, 'proProgrammer': 119238, 'theLooper': 138475, 'stringQueen': 85739}
