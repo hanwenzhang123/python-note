06c142c72352f20874934a706e6930527fbd7527
Hanwen Zhang <hanwenzhang888@gmail.com>
2021-06-04 17:56:17 -0400

    Update 04-constructor.py

--- oop-python/04-constructor.py
+++ oop-python/04-constructor.py
@@ -29,29 +29,4 @@ class Circle:
 teaching_table = Circle(36)
 #New circle with diameter: 36
 
-
-
-#Instance Variables
-#1
-class FakeDict:
-  pass
-fake_dict1 = FakeDict()
-fake_dict2 = FakeDict()
- 
-fake_dict1.fake_key = "This works!"
-fake_dict2.fake_key = "This too!"
- 
-# Let's join the two strings together!
-working_string = "{} {}".format(fake_dict1.fake_key, fake_dict2.fake_key)
-print(working_string)
-# prints "This works! This too!"
-
-#2
-class Store:
-  pass
-
-alternative_rocks = Store()
-isabelles_ices = Store()
-
-alternative_rocks.store_name = "Alternative Rocks"
-isabelles_ices.store_name = "Isabelle's Ices"
+  
