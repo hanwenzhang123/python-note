9f88663f2557130840da8c36dd0ad4e10c4030df
Hanwen Zhang <hanwenzhang888@gmail.com>
2021-06-04 17:41:59 -0400

    Create 01-type.py

---
+++ oop-python/01-type.py
@@ -0,0 +1,21 @@
+#types
+a_string = "Cool String"
+an_int = 12
+ 
+print(type(a_string))
+# prints "<class 'str'>"
+ 
+print(type(an_int))
+# prints "<class 'int'>"
+
+
+print(type(5))
+#<class 'int'>
+
+my_dict = {}
+print(type(my_dict))
+#<class 'dict'>
+
+my_list = []
+print(type(my_list))
+#<class 'list'>
