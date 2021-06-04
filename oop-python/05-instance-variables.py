5ef4e4b0a7a9e7cb9aabf9340af3147080db1a0f
Hanwen Zhang <hanwenzhang888@gmail.com>
2021-06-04 17:55:55 -0400

    Create 05-instance-variables.py

---
+++ oop-python/05-instance-variables.py
@@ -0,0 +1,25 @@
+#Instance Variables
+#1
+class FakeDict:
+  pass
+fake_dict1 = FakeDict()
+fake_dict2 = FakeDict()
+ 
+fake_dict1.fake_key = "This works!"
+fake_dict2.fake_key = "This too!"
+ 
+# Let's join the two strings together!
+working_string = "{} {}".format(fake_dict1.fake_key, fake_dict2.fake_key)
+print(working_string)
+# prints "This works! This too!"
+
+#2
+class Store:
+  pass
+
+alternative_rocks = Store()
+isabelles_ices = Store()
+
+alternative_rocks.store_name = "Alternative Rocks"
+isabelles_ices.store_name = "Isabelle's Ices"
+  
