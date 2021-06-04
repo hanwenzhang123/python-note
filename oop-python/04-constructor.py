be38cf6f4bec2a2cd0638b5b53acfa0f18738742
Hanwen Zhang <hanwenzhang888@gmail.com>
2021-06-04 17:54:59 -0400

    Create 04-constructor.py

---
+++ oop-python/04-constructor.py
@@ -0,0 +1,57 @@
+#The first dunder method we’re going to use is the __init__() method
+#1
+class Shouter:
+  def __init__(self, phrase):
+    # make sure phrase is a string
+    if type(phrase) == str:
+ 
+      # then shout it out
+      print(phrase.upper())
+ 
+shout1 = Shouter("shout")
+# prints "SHOUT"
+ 
+shout2 = Shouter("shout")
+# prints "SHOUT"
+ 
+shout3 = Shouter("let it all out")
+# prints "LET IT ALL OUT"
+
+
+#2
+class Circle:
+  pi = 3.14
+  
+  # Add constructor here:
+  def __init__(self, diameter):
+    print("New circle with diameter: {diameter}".format(diameter=diameter))
+    
+teaching_table = Circle(36)
+#New circle with diameter: 36
+
+
+
+#Instance Variables
+#1
+class FakeDict:
+  pass
+fake_dict1 = FakeDict()
+fake_dict2 = FakeDict()
+ 
+fake_dict1.fake_key = "This works!"
+fake_dict2.fake_key = "This too!"
+ 
+# Let's join the two strings together!
+working_string = "{} {}".format(fake_dict1.fake_key, fake_dict2.fake_key)
+print(working_string)
+# prints "This works! This too!"
+
+#2
+class Store:
+  pass
+
+alternative_rocks = Store()
+isabelles_ices = Store()
+
+alternative_rocks.store_name = "Alternative Rocks"
+isabelles_ices.store_name = "Isabelle's Ices"
