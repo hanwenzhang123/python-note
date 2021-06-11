4d8d265b9fd0b38ccadf12607e229792de317d54
Hanwen Zhang <hanwenzhang888@gmail.com>
2021-06-11 17:45:32 -0400

    Create 10-exception.py

---
+++ oop-python/10-exception.py
@@ -0,0 +1,70 @@
+An Exception is a class that inherits from Python’s Exception class.
+
+issubclass() is a Python built-in function that takes two parameters.
+issubclass() returns True if the first argument is a subclass of the second argument. 
+It returns False if the first class is not a subclass of the second. 
+issubclass() raises a TypeError if either argument passed in is not a class.
+
+
+issubclass(ZeroDivisionError, Exception)
+# Returns True
+
+
+class KitchenException(Exception):
+  """
+  Exception that gets thrown when a kitchen appliance isn't working
+  """
+ 
+class MicrowaveException(KitchenException):
+  """
+  Exception for when the microwave stops working
+  """
+ 
+class RefrigeratorException(KitchenException):
+  """
+  Exception for when the refrigerator stops working
+  """
+  
+def get_food_from_fridge():
+  if refrigerator.cooling == False:
+    raise RefrigeratorException
+  else:
+    return food
+ 
+def heat_food(food):
+  if microwave.working == False:
+    raise MicrowaveException
+  else:
+    microwave.cook(food)
+    return food
+ 
+try:
+  food = get_food_from_fridge()
+  food = heat_food(food)
+except KitchenException:
+  food = order_takeout()
+  
+  
+  
+  #2
+  # Define your exception up here:
+class OutOfStock(Exception):
+  pass
+
+# Update the class below to raise OutOfStock
+class CandleShop:
+  name = "Here's a Hot Tip: Buy Drip Candles"
+  def __init__(self, stock):
+    self.stock = stock
+    
+  def buy(self, color):
+    if self.stock[color] < 1:
+      raise OutOfStock
+    self.stock[color] = self.stock[color] - 1
+
+candle_shop = CandleShop({'blue': 6, 'red': 2, 'green': 0})
+candle_shop.buy('blue')
+
+# This should raise OutOfStock:
+# candle_shop.buy('green')
+  
