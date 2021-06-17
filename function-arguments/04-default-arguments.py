dd8cd2843cde13dfa7f8781121a36377e9235648
Hanwen Zhang <hanwenzhang888@gmail.com>
2021-06-17 11:37:55 -0400

    Update 04-default-arguments.py

--- function-arguments/04-default-arguments.py
+++ function-arguments/04-default-arguments.py
@@ -10,3 +10,69 @@ user1 = create_user('johnny_thunder', True)
  
 # Raises a "TypeError: Missing 1 required positional argument"
 user2 = create_user('djohansen')
+
+
+Above we defined our function, create_user, with two parameters. 
+We first called it with two arguments, but then tried calling it with only one argument and got an error.
+Not all function arguments in Python need to be required. 
+If we give a default argument to a Python function that argument won’t be required when we call the function.
+
+# Function defined with one required and one optional parameter
+def create_user(username, is_admin=False):
+  create_email(username)
+  set_permissions(is_admin)
+ 
+# Calling with two arguments uses the default value for is_admin
+user2 = create_user('djohansen')
+
+
+Above we defined create_user with a default argument for is_admin, so we can call that function with only the one argument 'djohansen'. 
+It assumes the default value for is_admin: False. We can make both of our parameters have a default value (therefore making them all optional).
+  
+  
+  # We can make all three parameters optional
+def create_user(username=None, is_admin=False):
+  if username is None:
+    username = "Guest"
+  else:
+    create_email(username)
+  set_permissions(is_admin)
+ 
+# So we can call with just one value
+user3 = create_user('ssylvain')
+# Or no value at all, which would create a Guest user
+user4 = create_user()
+
+
+
+# a function called make_folders(). We want to add a default argument to the nest parameter in make_folders().
+
+import os
+
+def make_folders(folders_list, nest=False):
+  if nest:
+    """
+    Nest all the folders, like
+    ./Music/fun/parliament
+    """
+    path_to_new_folder = "."
+    for folder in folders_list:
+      path_to_new_folder += "/{}".format(folder)
+      try:
+        print(path_to_new_folder)
+        os.makedirs("./" + path_to_new_folder)
+      except FileExistsError:
+        continue
+  else:
+    """
+    Makes all different folders, like
+    ./Music/ ./fun/ and ./parliament/
+    """
+    for folder in folders_list:
+      try:
+        os.makedirs(folder)
+
+      except FileExistsError:
+        continue
+
+make_folders(['Music', 'fun', 'parliament'])
