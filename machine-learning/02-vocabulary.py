a673d4ad37ec7657292c8113aafe64a5b73d0fb3
Hanwen Zhang <hanwenzhang888@gmail.com>
2021-06-30 16:04:56 -0400

    Update 02-vocabulary.py

--- machine-learning/02-vocabulary.py
+++ machine-learning/02-vocabulary.py
@@ -1,18 +1,37 @@
-#Machine Learning Framework
-Most approaches to machine learning can be described using statistical analysis and math, 
-which means that some of the components for a spam filter that classifies something as spam or not spam can be reapplied to another similar problem, 
-like classifying different types of zoo animals based on their physical appearance. 
-For this reason, there are many high level machine learning frameworks that already have all the low level code written, 
-so you can just plug your code and your data into pre-existing models.
+#Examples and Features
+Example: A single element in a dataset (sample)
+  A dataset is comprised on many examples. And in general, each example helps improve the confidence of your model's predictions.
 
-#Machine Learning Frameworks
-scikit-learn
-TensorFlow
-Machine Learning on AWS
-IBM Watson
-Microsoft Cognitive Toolkit
-Google's Cloud AI
-PyTorch
-Caffe2
-Keras
-Apache MXNet
+Feature: One characteristic of an example
+  If you formatted your data in a table, each feature might be a single column.
+  
+
+#Labels and Classifiers
+Label: A category for data, or a prediction from a classification algorithm 
+  like a label, spam or not spam
+  
+Classifier: A supervised machine learning model that makes a prediction about how a piece of data should be categorized
+  like a function, Data goes in, and then the classifier predicts the correct category for that data.
+
+There's an old saying in computing called garbage in, garbage out.
+It means that if you provide the computer with bad information,
+or if you give your machine learning model a data set that's inaccurate or not representative of the whole truth.
+Then you're going to get a bad result.
+
+  
+#Questions
+Which of the following best defines the term "feature" as it relates to machine learning?
+A feature is one characteristic of an example.
+
+Which of the following best defines the term "example" as it relates to machine learning?
+An example is a single element in a dataset.
+
+Which of the following best defines the term "classifier" as it relates to machine learning?
+A classifier is a type of algorithm or model that makes a prediction about how a piece of data should be categorized.
+
+Which of the following best defines the term "label" as it relates to machine learning?
+A label can be thought of as a category for data, or a prediction from a classification algorithm.
+
+A classifier attempts to assign a label.
+
+  
