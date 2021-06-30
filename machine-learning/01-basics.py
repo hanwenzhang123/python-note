45859f4dd800b67191e2d1ca466e26d6a01f7e08
Hanwen Zhang <hanwenzhang888@gmail.com>
2021-06-30 15:38:33 -0400

    Update 01-basics.py

--- machine-learning/01-basics.py
+++ machine-learning/01-basics.py
@@ -1,4 +1,37 @@
-What is machine learning?
+#What is machine learning?
 Machine learning encompasses many different ideas, programming languages, frameworks, and approaches to the subject, 
 so the term "machine learning" is difficult to define in just a sentence or two. 
 But essentially, machine learning is giving a computer the ability to write its own rules and learn about new things, on its own.
+
+
+#Supervised and Unsupervised Learning
+There are many different approaches or models in machine learning 
+but generally, they can be broken down into two major categories called supervised learning and unsupervised learning.
+
+Model: An algorithm or an approach to a problem
+  A model in machine learning is typically probabilistic, which does not produce an exact result.
+  
+Probability: A means of expressing how likely it is that an event will occur, or a way of measuring how close a value might be to the actual correct value
+  Value: 0 and 1
+    0 being complete guess, 1 being complete certain
+    
+This is relevant to machine learning because if a prediction is far outside the norm, 
+the model might have low confidence in the answer, because it's highly probable that it's not correct.
+If the prediction matches up almost perfectly with an existing example in a data set, then the confidence will be very high.
+
+
+Two Major Categories of Machine Learning:  
+1. Supervised learning: A case where a machine intelligence is tasked with predicting a category or a quantity
+
+Supervised learning is when a machine intelligence is tasked with predicting a category or a quantity.
+
+Classification: A supervised machine learning model that makes a prediction about how a piece of data should be categorized
+  e.g. email, spam email, promotional email
+Regression: A supervised machine learning model that attempts to predict a quantity or a number
+  e.g. predict the appropriate price by looking for the average price of a dish around the area of your newly open restaurant
+
+2. Unsupervised learning: A case where a computer analyzes unlabeled data and has no previous examples, and tries to identify patterns in the data
+Clustering: An unsupervised machine learning model that attempts to group similar examples together
+  e.g. suggested friends, suggested videos, advertisement sites
+  
+  
