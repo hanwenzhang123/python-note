8d31d998cbd143bcb64665521d199e01f62cd8e8
Hanwen Zhang <hanwenzhang888@gmail.com>
2021-06-30 16:10:52 -0400

    Create 04-review.py

---
+++ machine-learning/04-review.py
@@ -0,0 +1,15 @@
+Machine Learning is a new approach to artificial intelligence.
+With an emphasis on statistical analysis, and the ability for the computer to write
+its own set of rules, rather than humans writing all of the conditional logic.
+
+There are two major categories of Machine Learning, Supervised Learning and Unsupervised Learning.
+Supervised learning is when a machine intelligence predicts a category or a quantity using models of classification and regression respectively.
+Unsupervised learning is when a computer analyzes unlabeled data and attempts to recognize patterns.
+  The most common unsupervised models use clustering to group similar things together.
+
+When handling data, remember that an example is a single element in a dataset.
+And a feature is one characteristic of an example.
+
+These basic models can lead to more complex and emerging behaviors in specific domains like chatbots, image recognition, speech recognition and more.
+More specialized applications like this can be served by higher level machine learning platforms including AWS and IBM Watson.
+  
