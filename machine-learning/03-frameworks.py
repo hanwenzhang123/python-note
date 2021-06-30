6cbbcd5245d9131f7a7ddf2a31ea04c1d6c5122e
Hanwen Zhang <hanwenzhang888@gmail.com>
2021-06-30 15:40:50 -0400

    Create 03-frameworks.py

---
+++ machine-learning/03-frameworks.py
@@ -0,0 +1,18 @@
+#Machine Learning Framework
+Most approaches to machine learning can be described using statistical analysis and math, 
+which means that some of the components for a spam filter that classifies something as spam or not spam can be reapplied to another similar problem, 
+like classifying different types of zoo animals based on their physical appearance. 
+For this reason, there are many high level machine learning frameworks that already have all the low level code written, 
+so you can just plug your code and your data into pre-existing models.
+
+#Machine Learning Frameworks
+scikit-learn
+TensorFlow
+Machine Learning on AWS
+IBM Watson
+Microsoft Cognitive Toolkit
+Google's Cloud AI
+PyTorch
+Caffe2
+Keras
+Apache MXNet
