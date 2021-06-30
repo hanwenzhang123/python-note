ee25350fa8a5519ea8c73569074a41eb5d52c31e
Hanwen Zhang <hanwenzhang888@gmail.com>
2021-06-30 15:26:15 -0400

    Create 01-basics.py

---
+++ machine-learning/01-basics.py
@@ -0,0 +1,3 @@
+Machine learning encompasses many different ideas, programming languages, frameworks, and approaches to the subject, 
+so the term "machine learning" is difficult to define in just a sentence or two. 
+But essentially, machine learning is giving a computer the ability to write its own rules and learn about new things, on its own.
